f59e96e7726b19252a83d892dbf0e5e56dc48ad0
Jesús Quishpe <jesusquishpe17@gmail.com>
2022-08-30 12:05:01 -0500

    cambios dist

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="dist/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite + React</title>
-    <script type="module" crossorigin src="dist/assets/index.b17e1e7c.js"></script>
+    <title>Salutic</title>
+    <script type="module" crossorigin src="dist/assets/index.fe44dfeb.js"></script>
     <link rel="stylesheet" href="dist/assets/index.24270d4f.css">
   </head>
   <body>
