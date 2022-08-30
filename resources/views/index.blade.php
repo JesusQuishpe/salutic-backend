31bc47c16855b6f8020b4fa6958c82b6cdc63f15
Jesús Quishpe <jesusquishpe17@gmail.com>
2022-08-30 18:00:02 -0500

    Nuevos cambios en dist

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href={{url()+"/vite.svg"}} />
+    <link rel="icon" type="image/svg+xml" href={{url()+"/dist/vite.svg"}} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Salutic</title>
-    <script type="module" crossorigin src={{url()+"/assets/index.52ef63d4.js"}}></script>
-    <link rel="stylesheet" href={{url()+"/assets/index.24270d4f.css"}}>
+    <script type="module" crossorigin src={{url()+"dist/assets/index.08a5d97b.js"}}></script>
+    <link rel="stylesheet" href={{url()+"dist/assets/index.24270d4f.css"}}>
   </head>
   <body>
     <div id="root"></div>
