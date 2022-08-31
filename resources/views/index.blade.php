01e094ee99e5340b5dabb1ce99e4253a6b203f89
Jesús Quishpe <jesusquishpe17@gmail.com>
2022-08-30 19:16:39 -0500

    Nuevo cambios en public

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,14 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href={{url()+"/dist/vite.svg"}} />
+    <link rel="icon" type="image/svg+xml" href={{config('app.url','http://localhost/')."vite.svg"}} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Salutic</title>
-    <script type="module" crossorigin src={{url()+"dist/assets/index.08a5d97b.js"}}></script>
-    <link rel="stylesheet" href={{url()+"dist/assets/index.24270d4f.css"}}>
+    <script type="module" crossorigin src={{config('app.url','http://localhost/')."assets/index.08a5d97b.js"}}></script>
+    <link rel="stylesheet" href={{config('app.url','http://localhost/')."assets/index.24270d4f.css"}}>
   </head>
   <body>
     <div id="root"></div>
     <div id="loader"></div>
-    
   </body>
 </html>
